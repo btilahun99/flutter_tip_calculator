8ba78caca63f8c13a383c54993eb31e05c37d064
btilahun99 <98833771+btilahun99@users.noreply.github.com>
2022-03-27 17:49:16 -0500

    Add files via upload
    version 2

--- main.dart
+++ main.dart
@@ -14,15 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +24,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -52,10 +35,13 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   final amountTextField = TextEditingController();
-  final totalTipTextField  = TextEditingController();
+  //final totalTipTextField  = TextEditingController();
 
   double _billAmount = 0;
-  double _totalTip = 0;
+  //double _totalTip = 0;
+  String _totalTip = "";
+
+  void _calculateTip (){}
 
   void _incrementCounter() {
     setState(() {
@@ -88,19 +74,19 @@ class _MyHomePageState extends State<MyHomePage> {
               onChanged: (value) {
                 setState(() {
                   isSwitched = true;
-                  _totalTip = _billAmount *0.20;
+                  _totalTip = (_billAmount *0.20) as String;
                 });
                 Text("Great Service?");
               },
             ),
             ElevatedButton(onPressed: ()
-                  {
-                  _totalTip = double.parse(totalTipTextField.text);
-                  _totalTip = _billAmount *0.15;
-                  totalTipTextField.text = _totalTip.toStringAsFixed(2);
-                  },
+            {
+              _totalTip = double.parse(_totalTip) as String;
+              _totalTip = (_billAmount *0.15) as String;
+              //totalTipTextField.text = _totalTip.toStringAsFixed(2);
+            },
                 child: Text("Calculate Tip")),
-            TextField(controller: totalTipTextField),
+            Text("0"),
           ],
         ),
       ),
